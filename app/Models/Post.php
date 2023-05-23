311b607ade863cf77fcc7d273ad1cd8a5ddf4373
国吉広人 <hiroto.2530kuni@gmail.com>
2023-05-23 17:09:08 +0900

    削除

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
     protected $fillable = [
         'title',
@@ -16,6 +18,6 @@ class Post extends Model
         
     public function getPaginateBylimit(int $limit_count = 5)
     {
-        return $this->orderby('updated_at','DESC')->paginate($limit_count);
+        return $this->orderby('updated_at', 'DESC')->paginate($limit_count);
     }
 }
